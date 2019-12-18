76df8015c34f2f398547569a3aba3c0fc05afc61
Rajat Prakash <rajatprakash904@gmail.com>
2019-12-18 12:34:48 +0530

    Updated the task

--- testing.py
+++ testing.py
@@ -1,3 +1,8 @@
 # testing 1 is sucessfull, now working on 3 test.
 #testing 2 is going on -- changes done on github web
+Task to complete after this course is 
+  : API course
+  : Personal Assitant Application using python
+Dead line : 29 December
+   
 
