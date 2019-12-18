040352083a95a88f7d50f0cf958d945543046a17
Rajat Prakash <rajatprakash904@gmail.com>
2019-12-18 12:31:40 +0530

    3 testing is going on

--- testing.py
+++ testing.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 # testing 1 is sucessfull, now working on 3 test.
-=======
-# testing 1 is going on
 #testing 2 is going on -- changes done on github web
->>>>>>> 84ed1a116aad95813aa9ecfd9dbc7a616608b2d8
+
